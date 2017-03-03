2ca948b271b64625dd184409da5a3cdc4ddb1822
maknahar <maknahar@yahoo.in>
2017-03-03 23:49:36 +0530

    Added conversational patterns

--- main.go
+++ main.go
@@ -12,13 +12,14 @@ import (
 var SLACK_TOKEN = os.Getenv("SLACK_TOKEN")
 
 func main() {
+
 	if SLACK_TOKEN == "" {
 		log.Fatal("Missing SLACK_TOKEN env var")
 	}
 
-	fmt.Println(SLACK_TOKEN)
-
 	api := slack.New(SLACK_TOKEN)
+	api.SetDebug(true)
+
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
 
@@ -37,7 +38,7 @@ Loop:
 				prefix := fmt.Sprintf("<@%s> ", info.User.ID)
 
 				if ev.User != info.User.ID && strings.HasPrefix(ev.Text, prefix) {
-					rtm.SendMessage(rtm.NewOutgoingMessage("What's up buddy!?!?", ev.Channel))
+					respond(rtm, ev, prefix)
 				}
 
 			case *slack.RTMError:
@@ -51,5 +52,33 @@ Loop:
 				//Take no action
 			}
 		}
+	}
+}
+
+func respond(rtm *slack.RTM, msg *slack.MessageEvent, prefix string) {
+	var response string
+	text := msg.Text
+	text = strings.TrimPrefix(text, prefix)
+	text = strings.TrimSpace(text)
+	text = strings.ToLower(text)
+
+	acceptedGreetings := map[string]bool{
+		"what's up?": true,
+		"hey!":       true,
+		"yo":         true,
+		"hi!":        true,
+	}
+	acceptedHowAreYou := map[string]bool{
+		"how's it going?": true,
+		"how are ya?":     true,
+		"feeling okay?":   true,
+	}
+
+	if acceptedGreetings[text] {
+		response = "What's up buddy!?!?!"
+		rtm.SendMessage(rtm.NewOutgoingMessage(response, msg.Channel))
+	} else if acceptedHowAreYou[text] {
+		response = "Good. How are you?"
+		rtm.SendMessage(rtm.NewOutgoingMessage(response, msg.Channel))
 	}
 }
